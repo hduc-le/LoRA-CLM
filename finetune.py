fb39c87f956b751b517b301168392704bf414922
hduc-le <hduc.lee@gmail.com>
2023-05-21 10:37:45 +0700

    reformat and add deepspeed utilities

--- finetune.py
+++ finetune.py
@@ -10,9 +10,10 @@ from accelerate import Accelerator
 from colorlog import ColoredFormatter
 from utils.read import read_config, get_model_tokenizer
 from utils.data import generate_prompt, print_trainable_parameters
-from transformers import DataCollatorForLanguageModeling, set_seed, get_scheduler
+from transformers import DataCollatorForLanguageModeling, set_seed, get_scheduler, AdamW
 from torch.utils.data import DataLoader
-from peft import LoraConfig, get_peft_model
+from accelerate.utils import DummyScheduler, DummyOptim, set_seed
+from peft import LoraConfig, get_peft_model, TaskType
 
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
@@ -40,25 +41,32 @@ logger.handlers = []  # No duplicated handlers
 logger.propagate = False  # workaround for duplicated logs in ipython
 logger.addHandler(ch)
 
+
 def train(config):
     # Load model and tokenizer
-    model, tokenizer = get_model_tokenizer(config["model"]["name"], 
-                                           load_in_8bit=config["model"]["load_in_8bit"], 
-                                           gradient_checkpointing=config["train"]["gradient_checkpointing"])
+    model, tokenizer = get_model_tokenizer(
+        config["model"]["name"],
+        load_in_8bit=config["model"]["load_in_8bit"],
+        gradient_checkpointing=config["train"]["gradient_checkpointing"],
+    )
     # LoRA fine tune
     if config["lora"]["active"]:
         # Peft model
-        peft_config = LoraConfig(r=config["lora"]["r"],
-                                 inference_mode=False,
-                                 lora_alpha=config["lora"]["alpha"],
-                                 lora_dropout=config["lora"]["dropout"],
-                                 bias="none",
-                                 task_type="CAUSAL_LM")
-        
+        peft_config = LoraConfig(
+            r=config["lora"]["r"],
+            inference_mode=False,
+            lora_alpha=config["lora"]["alpha"],
+            lora_dropout=config["lora"]["dropout"],
+            bias="none",
+            task_type=TaskType.CAUSAL_LM,
+        )
+
         model = get_peft_model(model, peft_config)
         print_trainable_parameters(model)
 
-    logger.info(f"Mem needed: {model.get_memory_footprint() / 1024 / 1024 / 1024:.2f} GB")
+    logger.info(
+        f"Mem needed: {model.get_memory_footprint() / 1024 / 1024 / 1024:.2f} GB"
+    )
 
     # Load datasetimport numpy as np
     data = load_dataset("json", data_files=config["data"]["path"], split="train")
@@ -69,46 +77,85 @@ def train(config):
             max_length=config["data"]["max_length"],
             padding="max_length",
         ),
-        remove_columns=["instruction", "input", "response"]
+        remove_columns=["instruction", "input", "response"],
     )
     # 90% train, 10% test + validation
-    split_dataset = data.train_test_split(test_size=config["data"]["test_size"], seed=config["train"]["seed"])
-    
+    split_dataset = data.train_test_split(
+        test_size=config["data"]["test_size"], seed=config["train"]["seed"]
+    )
+
     # Initialize accelerator
-    accelerator = Accelerator(gradient_accumulation_steps=config["train"]["gradient_accumulation_steps"])
+    accelerator = Accelerator(
+        gradient_accumulation_steps=config["train"]["gradient_accumulation_steps"]
+    )
     accelerator.print(f"Using {accelerator.num_processes} GPUs")
-    
+
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
-    train_dataloader = DataLoader(split_dataset["train"], 
-                                  batch_size=config["data"]["train_batch_size"], 
-                                  collate_fn=data_collator,
-                                  shuffle=True)
-    
-    eval_dataloader = DataLoader(split_dataset["test"], 
-                                 batch_size=config["data"]["eval_batch_size"], 
-                                 collate_fn=data_collator,
-                                 shuffle=False)
+    train_dataloader = DataLoader(
+        split_dataset["train"],
+        batch_size=config["data"]["train_batch_size"],
+        collate_fn=data_collator,
+        shuffle=True,
+    )
 
-    optimizer = torch.optim.AdamW(model.parameters(), 
-                                  lr=config["optim"]["lr"] * accelerator.num_processes, 
-                                  weight_decay=config["optim"]["weight_decay"])
-    
+    eval_dataloader = DataLoader(
+        split_dataset["test"],
+        batch_size=config["data"]["eval_batch_size"],
+        collate_fn=data_collator,
+        shuffle=False,
+    )
+
+    optimizer_cls = (
+        AdamW
+        if accelerator.state.deepspeed_plugin is None
+        or "optimizer" not in accelerator.state.deepspeed_plugin.deepspeed_config
+        else DummyOptim
+    )
+
+    optimizer = optimizer_cls(
+        model.parameters(),
+        lr=config["optim"]["lr"],
+        weight_decay=config["optim"]["weight_decay"],
+    )
+
+    gradient_accumulation_steps = config["train"]["gradient_accumulation_steps"]
+    if accelerator.state.deepspeed_plugin is not None:
+        gradient_accumulation_steps = (
+            accelerator.state.deepspeed_plugin.deepspeed_config[
+                "gradient_accumulation_steps"
+            ]
+        )
+
     # decay to min_lr instead of 0
     lr_ratio = config["optim"]["min_lr"] / config["optim"]["lr"]
     accelerator.print(f"Len of train_dataloader: {len(train_dataloader)}")
-    total_num_steps = (len(train_dataloader) / config["train"]["gradient_accumulation_steps"]) * config["train"]["num_epochs"]
+    total_num_steps = (len(train_dataloader) / gradient_accumulation_steps) * config[
+        "train"
+    ]["num_epochs"]
     # instead of decaying to zero, decay to ratio of min_lr / lr
     total_num_steps += int(total_num_steps * lr_ratio) + config["optim"]["warmup_steps"]
     accelerator.print(f"Total training steps: {total_num_steps}")
 
-    scheduler = get_scheduler(
+    # Creates Dummy Scheduler if `scheduler` was specified in the config file else creates `args.lr_scheduler_type` Scheduler
+    if (
+        accelerator.state.deepspeed_plugin is None
+        or "scheduler" not in accelerator.state.deepspeed_plugin.deepspeed_config
+    ):
+        scheduler = get_scheduler(
             name="cosine",
             optimizer=optimizer,
-            num_warmup_steps=config["warmup_steps"] * accelerator.num_processes,
+            num_warmup_steps=config["optim"]["warmup_steps"]
+            * accelerator.num_processes,
             num_training_steps=total_num_steps,
         )
-    
+    else:
+        scheduler = DummyScheduler(
+            optimizer,
+            total_num_steps=config["optim"]["warmup_steps"],
+            warmup_num_steps=config["optim"]["warmup_steps"],
+        )
+
     # To have only one message (and not 8) per logs of Transformers or Datasets, we set the logging verbosity
     # to INFO for the main process only.
     if accelerator.is_main_process:
@@ -117,22 +164,34 @@ def train(config):
     else:
         datasets.utils.logging.set_verbosity_error()
         transformers.utils.logging.set_verbosity_error()
-        
+
     # The seed need to be set before we instantiate the model, as it will determine the random head.
     set_seed(config["train"]["seed"])
 
     # There is no specific order to remember, we just need to unpack the objects in the same order we gave them to the
     # prepare method.
-    model, optimizer, scheduler, train_dataloader, eval_dataloader = accelerator.prepare(
+    (
+        model,
+        optimizer,
+        scheduler,
+        train_dataloader,
+        eval_dataloader,
+    ) = accelerator.prepare(
         model, optimizer, scheduler, train_dataloader, eval_dataloader
     )
-    
+
     # Now we train the model
     epochs_no_improve = 0
     min_val_loss = np.inf
     for epoch in range(config["train"]["num_epochs"]):
         model.train()
-        for batch in (pbar:=tqdm(train_dataloader, desc=f"Epoch {epoch} - training", disable=not accelerator.is_main_process)):
+        for batch in (
+            pbar := tqdm(
+                train_dataloader,
+                desc=f"Epoch {epoch} - Training",
+                disable=not accelerator.is_main_process,
+            )
+        ):
             with accelerator.accumulate(model):
                 outputs = model(**batch)
                 loss = outputs.loss
@@ -141,13 +200,19 @@ def train(config):
                 optimizer.step()
                 scheduler.step()
                 optimizer.zero_grad()
-                
+
                 pbar.set_postfix({"loss": loss.item()})
 
         # Evaluate at the end of the epoch (distributed evaluation as we have 8 TPU cores)
         model.eval()
         validation_losses = []
-        for batch in (pbar:=tqdm(eval_dataloader, desc="Epoch {epoch} - validation", disable=not accelerator.is_main_process)):
+        for batch in (
+            pbar := tqdm(
+                eval_dataloader,
+                desc="Epoch {epoch} - Validation",
+                disable=not accelerator.is_main_process,
+            )
+        ):
             with torch.no_grad():
                 outputs = model(**batch)
             loss = outputs.loss
@@ -162,14 +227,16 @@ def train(config):
         if val_loss < min_val_loss:
             epochs_no_improve = 0
             min_val_loss = val_loss
-            
+
             accelerator.print(f"Epoch {epoch} finished")
             accelerator.print(f"Pushing to HF hub")
             accelerator.wait_for_everyone()
             unwrapped_model = accelerator.unwrap_model(model)
             try:
                 if accelerator.is_main_process:
-                    unwrapped_model.push_to_hub(config["model"]["save_name"] + f"-epoch_{epoch}", private=True)
+                    unwrapped_model.push_to_hub(
+                        config["model"]["save_name"] + f"-epoch_{epoch}", private=True
+                    )
 
             except Exception as e:
                 accelerator.print(e)
@@ -201,10 +268,10 @@ def train(config):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Training Arguments')
-    parser.add_argument('--config', type=str, default="configs/finetune.yaml")
+    parser = argparse.ArgumentParser(description="Training Arguments")
+    parser.add_argument("--config", type=str, default="configs/finetune.yaml")
     args = parser.parse_args()
-    
+
     # Load the training config file
     config = read_config(args.config)
     train(config)
