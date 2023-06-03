cf14ef40a4795929bcd121e5d165d5883571f707
hduc-le <hduc.lee@gmail.com>
2023-06-03 14:38:35 +0700

    rm ds & add fsdp

--- finetune.py
+++ finetune.py
@@ -5,6 +5,7 @@ import numpy as np
 import logging
 import datasets
 import transformers
+from torchmetrics import MeanMetric
 from tqdm import tqdm
 from datasets import load_dataset
 from accelerate import Accelerator
@@ -13,35 +14,14 @@ from utils.read import read_config, get_model_tokenizer
 from utils.data import generate_prompt, print_trainable_parameters
 from transformers import DataCollatorForLanguageModeling, set_seed, get_scheduler, AdamW
 from torch.utils.data import DataLoader
-from accelerate.utils import DummyScheduler, DummyOptim, set_seed
-from peft import LoraConfig, get_peft_model, TaskType
+from accelerate.utils import set_seed
+from peft.utils.other import fsdp_auto_wrap_policy
+from peft import LoraConfig, get_peft_model, TaskType, PeftModel
 
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+from accelerate.logging import get_logger
+logger = get_logger(__name__, log_level="DEBUG")
 
-formatter = ColoredFormatter(
-    "%(log_color)s[%(asctime)s] %(message)s",
-    datefmt=None,
-    reset=True,
-    log_colors={
-        "DEBUG": "cyan",
-        "INFO": "green,bold",
-        "INFOV": "cyan,bold",
-        "WARNING": "yellow",
-        "ERROR": "red,bold",
-        "CRITICAL": "red,bg_white",
-    },
-    secondary_log_colors={},
-    style="%",
-)
-ch.setFormatter(formatter)
-
-logger = logging.getLogger("rn")
-logger.setLevel(logging.DEBUG)
-logger.handlers = []  # No duplicated handlers
-logger.propagate = False  # workaround for duplicated logs in ipython
-logger.addHandler(ch)
-
+torch.backends.cuda.matmul.allow_tf32 = True
 
 def train(config):
     # Load model and tokenizer
@@ -52,22 +32,28 @@ def train(config):
     )
     # LoRA fine tune
     if config["lora"]["active"]:
-        # Peft model
-        peft_config = LoraConfig(
-            r=config["lora"]["r"],
-            inference_mode=False,
-            lora_alpha=config["lora"]["alpha"],
-            lora_dropout=config["lora"]["dropout"],
-            bias="none",
-            task_type=TaskType.CAUSAL_LM,
-        )
+        if config["lora"]["pretrained_path"]:
+            model = PeftModel.from_pretrained(model, config["lora"]["pretrained_path"], is_trainable=True)
 
-        model = get_peft_model(model, peft_config)
+        else:
+            peft_config = LoraConfig(
+                r=config["lora"]["r"],
+                inference_mode=False,
+                lora_alpha=config["lora"]["alpha"],
+                lora_dropout=config["lora"]["dropout"],
+                bias="none",
+                task_type=TaskType.CAUSAL_LM,
+            )
+            model = get_peft_model(model, peft_config)
         print_trainable_parameters(model)
+    
+    logger.info(f"Mem needed: {model.get_memory_footprint() / 1024 / 1024 / 1024:.2f} GB", main_process_only=True)
 
-    logger.info(
-        f"Mem needed: {model.get_memory_footprint() / 1024 / 1024 / 1024:.2f} GB"
-    )
+    # FSDP plugin with accelerate
+    if getattr(accelerator.state, "fsdp_plugin", None) is not None:
+        accelerator.state.fsdp_plugin.auto_wrap_policy = fsdp_auto_wrap_policy(model)
+    # When using FSDP, it is efficient and recommended to call prepare for the model before creating the optimizer
+    model = accelerator.prepare(model)
 
     # Load datasetimport numpy as np
     data = load_dataset("json", data_files=config["data"]["path"], split="train")
@@ -90,7 +76,7 @@ def train(config):
     accelerator = Accelerator(
         gradient_accumulation_steps=config["train"]["gradient_accumulation_steps"]
     )
-    accelerator.print(f"Using {accelerator.num_processes} GPUs")
+    logger.info(f"Using {accelerator.num_processes} GPUs", main_process_only=True)
 
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
@@ -108,55 +94,29 @@ def train(config):
         shuffle=False,
     )
 
-    optimizer_cls = (
-        AdamW
-        if accelerator.state.deepspeed_plugin is None
-        or "optimizer" not in accelerator.state.deepspeed_plugin.deepspeed_config
-        else DummyOptim
-    )
-
-    optimizer = optimizer_cls(
+    optimizer = AdamW(
         model.parameters(),
         lr=config["optim"]["lr"],
         weight_decay=config["optim"]["weight_decay"],
     )
 
     gradient_accumulation_steps = config["train"]["gradient_accumulation_steps"]
-    if accelerator.state.deepspeed_plugin is not None:
-        gradient_accumulation_steps = (
-            accelerator.state.deepspeed_plugin.deepspeed_config[
-                "gradient_accumulation_steps"
-            ]
-        )
 
     # decay to min_lr instead of 0
     lr_ratio = config["optim"]["min_lr"] / config["optim"]["lr"]
     accelerator.print(f"Len of train_dataloader: {len(train_dataloader)}")
-    total_num_steps = (len(train_dataloader) / gradient_accumulation_steps) * config[
-        "train"
-    ]["num_epochs"]
+    total_num_steps = (len(train_dataloader) / gradient_accumulation_steps) * config["train"]["num_epochs"]
     # instead of decaying to zero, decay to ratio of min_lr / lr
     total_num_steps += int(total_num_steps * lr_ratio) + config["optim"]["warmup_steps"]
     accelerator.print(f"Total training steps: {total_num_steps}")
 
-    # Creates Dummy Scheduler if `scheduler` was specified in the config file else creates `args.lr_scheduler_type` Scheduler
-    if (
-        accelerator.state.deepspeed_plugin is None
-        or "scheduler" not in accelerator.state.deepspeed_plugin.deepspeed_config
-    ):
-        scheduler = get_scheduler(
-            name="cosine",
-            optimizer=optimizer,
-            num_warmup_steps=config["optim"]["warmup_steps"]
-            * accelerator.num_processes,
-            num_training_steps=total_num_steps,
-        )
-    else:
-        scheduler = DummyScheduler(
-            optimizer,
-            total_num_steps=config["optim"]["warmup_steps"],
-            warmup_num_steps=config["optim"]["warmup_steps"],
-        )
+    scheduler = get_scheduler(
+        name="cosine",
+        optimizer=optimizer,
+        num_warmup_steps=config["optim"]["warmup_steps"]
+        * accelerator.num_processes,
+        num_training_steps=total_num_steps,
+    )
 
     # To have only one message (and not 8) per logs of Transformers or Datasets, we set the logging verbosity
     # to INFO for the main process only.
@@ -172,60 +132,57 @@ def train(config):
 
     # There is no specific order to remember, we just need to unpack the objects in the same order we gave them to the
     # prepare method.
-    (
-        model,
-        optimizer,
-        scheduler,
-        train_dataloader,
-        eval_dataloader,
-    ) = accelerator.prepare(
-        model, optimizer, scheduler, train_dataloader, eval_dataloader
+    optimizer, scheduler, train_dataloader, eval_dataloader = accelerator.prepare(
+        optimizer, scheduler, train_dataloader, eval_dataloader
     )
 
     # Now we train the model
     epochs_no_improve = 0
     min_val_loss = np.inf
     for epoch in range(config["train"]["num_epochs"]):
-        model.train()
-        for batch in (
-            pbar := tqdm(
-                train_dataloader,
-                desc=f"Epoch {epoch} - Training",
-                disable=not accelerator.is_main_process,
-            )
-        ):
-            with accelerator.accumulate(model):
-                outputs = model(**batch)
-                loss = outputs.loss
-                accelerator.backward(loss)
+        train_loss = MeanMetric(nan_strategy="error").to(model.device)
+        for step, batch in enumerate((pbar := tqdm(train_dataloader, desc=f"Epoch {epoch} - Training", disable=not accelerator.is_main_process))):
+            model.train()
+            outputs = model(**batch)
+            loss = outputs.loss
 
+            # gather loss before backprop in case of gradient accumulation
+            loss_values = accelerator.gather_for_metrics({"loss": loss.detach().float()})
+            train_loss.update(loss_values["loss"])
+
+            # Gradient accumulation and backprop
+            loss = loss / gradient_accumulation_steps
+            accelerator.backward(loss)
+
+            if (step + 1) % gradient_accumulation_steps == 0 or step == len(train_dataloader) - 1:
                 optimizer.step()
                 scheduler.step()
                 optimizer.zero_grad()
 
-                pbar.set_postfix({"loss": loss.item()})
+            pbar.set_postfix({"loss": loss_values["loss"]})
 
-        # Evaluate at the end of the epoch (distributed evaluation as we have 8 TPU cores)
+        # Evaluate at the end of the epoch (distributed evaluation as we have all GPU cores)
         model.eval()
-        validation_losses = []
-        for batch in (
-            pbar := tqdm(
-                eval_dataloader,
-                desc=f"Epoch {epoch} - Validation",
-                disable=not accelerator.is_main_process,
-            )
-        ):
-            with torch.no_grad():
-                outputs = model(**batch)
-            loss = outputs.loss
-            pbar.set_postfix({"loss": loss.item()})
-            # We gather the loss from the 8 TPU cores to have them all.
-            validation_losses.append(accelerator.gather(loss[None]))
+        val_loss = MeanMetric(nan_strategy="error").to(model.device)
+        with torch.no_grad():
+            for batch in (pbar := tqdm(eval_dataloader, desc=f"Epoch {epoch} - Validation", disable=not accelerator.is_main_process)):
+                loss = model(**batch).loss
 
-        # Compute average validation loss
-        val_loss = torch.stack(validation_losses).sum().item() / len(validation_losses)
+                loss_values = accelerator.gather_for_metrics({"loss": loss.detach()})
+
+                val_loss.update(loss_values["loss"])
+
+                pbar.set_postfix({"loss": loss_values["loss"]})
+
+        # Compute average train and validation loss
+        log_items = {
+            "train_loss": train_loss.compute(),
+            "val_loss": val_loss.compute()
+        }
+
         # Use accelerator.print to print only on the main process.
-        accelerator.print(f"epoch {epoch}: validation loss:", val_loss)
+        accelerator.print(f"Summary epoch {epoch}: train loss: {log_items['train_loss']} || validation loss: {log_items['val_loss']}")
+
         if val_loss < min_val_loss:
             epochs_no_improve = 0
             min_val_loss = val_loss
@@ -260,17 +217,23 @@ def train(config):
                 accelerator.print("Early stopping!")
                 break
 
+        train_loss.reset()
+
+    save_dir = f"{config['model']['output_dir']}/final"
+    logger.info("Training finished.", main_process_only=True)
+    logger.info(f"Unpacking and saving the final checkpoint at {save_dir}", main_process_only=True)
+
     # save trained model
     accelerator.wait_for_everyone()
     unwrapped_model = accelerator.unwrap_model(model)
     # Use accelerator.save to save
     unwrapped_model.save_pretrained(
-        f"{config['model']['output_dir']}/final",
+        save_dir,
         is_main_process=accelerator.is_main_process,
         save_function=accelerator.save,
         state_dict=accelerator.get_state_dict(model),
     )
-
+    accelerator.print("Done.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Training Arguments")
